28383c3fd174f80b8a0f8c6f0e829e74e7c0a89f
Vijay Singh <vijay@tchowklabs.com>
2014-06-06 18:58:33 +0530

    First commit make robomate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robomate::Application.config.secret_token = 'b4d8be2ec3be695763bb50087aa0fadb11b11b5d33e8519f841177ec50df8403fc65b39cc759bf76cec7546b4c24cec14c1099e90988d9d52a604f3da4bbf112'
