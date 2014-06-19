ffc93ead331083b3873ef99db6fb473e5228e223
Vijay Singh <vijay@tchowklabs.com>
2014-06-20 04:12:37 +0530

    changes routes

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Robomate::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   authenticated :user do
-    root :to => 'users#index'
+    root :to => 'welcome#index'
   end
     root :to => 'welcome#index'
 
